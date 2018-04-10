83f977f65c63d11c1399d553b6e84a768a4b0ab3
Kevin William Baylón Huerta <kbaylonh@outlook.com>
2018-04-10 13:30:14 -0500

    Update google-login-provider.ts
    This change solved issue #67

--- src/lib/src/providers/google-login-provider.ts
+++ src/lib/src/providers/google-login-provider.ts
@@ -15,7 +15,7 @@ export class GoogleLoginProvider extends BaseLoginProvider {
   initialize(): Promise<SocialUser> {
     return new Promise((resolve, reject) => {
       this.loadScript(GoogleLoginProvider.PROVIDER_ID,
-        '//apis.google.com/js/platform.js',
+        '//apis.google.com/js/api.js',
         () => {
           gapi.load('auth2', () => {
             this.auth2 = gapi.auth2.init({
