98a9fc2e939dda3e4feecc00e0284d9a6cf9643e
Josh Dague <daguej@email.uc.edu>
2018-09-11 13:53:21 -0700

    Load Google platform.js over HTTPS
    apis.google.com uses HSTS; the JS must be loaded over HTTPS.

--- src/lib/src/providers/google-login-provider.ts
+++ src/lib/src/providers/google-login-provider.ts
@@ -15,7 +15,7 @@ export class GoogleLoginProvider extends BaseLoginProvider {
   initialize(): Promise<void> {
     return new Promise((resolve, reject) => {
       this.loadScript(GoogleLoginProvider.PROVIDER_ID,
-        '//apis.google.com/js/platform.js',
+        'https://apis.google.com/js/platform.js',
         () => {
           gapi.load('auth2', () => {
             this.auth2 = gapi.auth2.init({
