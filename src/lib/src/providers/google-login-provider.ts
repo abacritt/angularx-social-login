0c0e962ca5dd60a7b241390758c056e065b386dc
Jaibatrik Dutta <jaibatrik@gmail.com>
2018-11-25 08:52:13 +0530

    Merged pending PRs, Angular 7 upgrade

--- src/lib/src/providers/google-login-provider.ts
+++ src/lib/src/providers/google-login-provider.ts
@@ -6,100 +6,106 @@ declare let gapi: any;
 
 export class GoogleLoginProvider extends BaseLoginProvider {
 
-	public static readonly PROVIDER_ID: string = 'GOOGLE';
+    public static readonly PROVIDER_ID: string = 'GOOGLE';
 
-	protected auth2: any;
+    protected auth2: any;
 
-	constructor(private clientId: string, private opt: LoginOpt = { scope: 'email' }) { super(); }
+    constructor(private clientId: string, private opt: LoginOpt = { scope: 'email' }) { super(); }
 
-  initialize(): Promise<void> {
-    return new Promise((resolve, reject) => {
-      this.loadScript(GoogleLoginProvider.PROVIDER_ID,
-        'https://apis.google.com/js/platform.js',
-        () => {
-          gapi.load('auth2', () => {
-            this.auth2 = gapi.auth2.init({
-              ...this.opt,
-              client_id: this.clientId
+    initialize(): Promise<void> {
+        return new Promise((resolve, reject) => {
+            this.loadScript(GoogleLoginProvider.PROVIDER_ID,
+                'https://apis.google.com/js/platform.js',
+                () => {
+                    gapi.load('auth2', () => {
+                        this.auth2 = gapi.auth2.init({
+                            ...this.opt,
+                            client_id: this.clientId
+                        });
+
+                        this.auth2.then(() => {
+                            this._readyState.next(true);
+                            resolve();
+                        }).catch((err: any) => {
+                            reject(err);
+                        });
+                    });
+                });
+        });
+    }
+
+    getLoginStatus(): Promise<SocialUser> {
+        return new Promise((resolve, reject) => {
+            this.onReady().then(() => {
+                if (this.auth2.isSignedIn.get()) {
+                    let user: SocialUser = new SocialUser();
+                    let profile = this.auth2.currentUser.get().getBasicProfile();
+                    let token = this.auth2.currentUser.get().getAuthResponse(true).access_token;
+                    let backendToken = this.auth2.currentUser.get().getAuthResponse(true).id_token;
+
+                    user.id = profile.getId();
+                    user.name = profile.getName();
+                    user.email = profile.getEmail();
+                    user.photoUrl = profile.getImageUrl();
+                    user.firstName = profile.getGivenName();
+                    user.lastName = profile.getFamilyName();
+                    user.authToken = token;
+                    user.idToken = backendToken;
+                    resolve(user);
+                }
             });
+        });
+    }
 
-						this.auth2.then(() => {
-							this._readyState.next(true);
-							resolve();
-						}).catch((err: any) => {
-							reject(err);
-						});
-					});
-				});
-		});
-	}
+    signIn(opt?: LoginOpt): Promise<SocialUser> {
+        return new Promise((resolve, reject) => {
+            this.onReady().then(() => {
+                let promise = this.auth2.signIn(opt);
 
-	getLoginStatus(): Promise<SocialUser> {
-		return new Promise((resolve, reject) => {
-			this.onReady().then(() => {
-				if (this.auth2.isSignedIn.get()) {
-					let user: SocialUser = new SocialUser();
-					let profile = this.auth2.currentUser.get().getBasicProfile();
-					let token = this.auth2.currentUser.get().getAuthResponse(true).access_token;
-					let backendToken = this.auth2.currentUser.get().getAuthResponse(true).id_token;
+                promise.then(() => {
+                    let user: SocialUser = new SocialUser();
+                    let profile = this.auth2.currentUser.get().getBasicProfile();
+                    let token = this.auth2.currentUser.get().getAuthResponse(true).access_token;
+                    let backendToken = this.auth2.currentUser.get().getAuthResponse(true).id_token;
 
-					user.id = profile.getId();
-					user.name = profile.getName();
-					user.email = profile.getEmail();
-					user.photoUrl = profile.getImageUrl();
-					user.firstName = profile.getGivenName();
-					user.lastName = profile.getFamilyName();
-					user.authToken = token;
-					user.idToken = backendToken;
-					resolve(user);
-				}
-			});
-		});
-	}
+                    user.id = profile.getId();
+                    user.name = profile.getName();
+                    user.email = profile.getEmail();
+                    user.photoUrl = profile.getImageUrl();
+                    user.firstName = profile.getGivenName();
+                    user.lastName = profile.getFamilyName();
+                    user.authToken = token;
+                    user.idToken = backendToken;
+                    resolve(user);
+                }, (closed: any) => {
+                    reject('User cancelled login or did not fully authorize.');
+                }).catch((err: any) => {
+                    reject(err);
+                });
+            });
+        });
+    }
 
-	signIn(opt?: LoginOpt): Promise<SocialUser> {
-		return new Promise((resolve, reject) => {
-			this.onReady().then(() => {
-				let promise = this.auth2.signIn(opt);
+    signOut(revoke?: boolean): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.onReady().then(() => {
+                let signOutPromise;
+                if (revoke) {
+                    signOutPromise = this.auth2.disconnect();
+                } else {
+                    signOutPromise = this.auth2.signOut();
+                }
 
-				promise.then(() => {
-					let user: SocialUser = new SocialUser();
-					let profile = this.auth2.currentUser.get().getBasicProfile();
-					let token = this.auth2.currentUser.get().getAuthResponse(true).access_token;
-					let backendToken = this.auth2.currentUser.get().getAuthResponse(true).id_token;
-
-					user.id = profile.getId();
-					user.name = profile.getName();
-					user.email = profile.getEmail();
-					user.photoUrl = profile.getImageUrl();
-					user.firstName = profile.getGivenName();
-					user.lastName = profile.getFamilyName();
-					user.authToken = token;
-					user.idToken = backendToken;
-					resolve(user);
-				}, (closed: any) => {
-					reject('User cancelled login or did not fully authorize.');
-				}).catch((err: any) => {
-					reject(err);
-				});
-			});
-		});
-	}
-
-	signOut(revoke?: boolean): Promise<any> {
-		return new Promise((resolve, reject) => {
-			this.onReady().then(() => {
-				<Promise<any>>(revoke ? this.auth2.disconnect() : this.auth2.signOut())
-					.then((err: any) => {
-						if (err) {
-							reject(err);
-						} else {
-							resolve();
-						}
-					}).catch((err: any) => {
-						reject(err);
-					});
-			});
-		});
-	}
+                signOutPromise.then((err: any) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve();
+                    }
+                }).catch((err: any) => {
+                    reject(err);
+                });
+            });
+        });
+    }
 }
