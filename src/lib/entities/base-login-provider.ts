caf936464c761d532e06ee65c3e05419528bae29
Jaibatrik Dutta <jaibatrik@gmail.com>
2019-10-13 15:41:41 +0530

    Fixes #204

--- src/lib/entities/base-login-provider.ts
+++ src/lib/entities/base-login-provider.ts
@@ -1,16 +1,18 @@
+import { Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { LoginProvider } from './login-provider';
 import { SocialUser } from './user';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 
 export abstract class BaseLoginProvider implements LoginProvider {
 
     protected _readyState: BehaviorSubject<boolean> = new BehaviorSubject(false);
 
-    constructor() { }
+    constructor(@Inject(PLATFORM_ID) private platformId?: Object) { }
 
     protected onReady(): Promise<void> {
         return new Promise((resolve, reject) => {
-            this._readyState.subscribe((isReady) => {
+            this._readyState.subscribe((isReady: boolean) => {
                 if (isReady) {
                     resolve();
                 }
@@ -24,13 +26,14 @@ export abstract class BaseLoginProvider implements LoginProvider {
     abstract signOut(revoke?: boolean): Promise<any>;
 
     loadScript(id: string, src: string, onload: any, async = true, inner_text_content = ''): void {
-        if (document.getElementById(id)) { return; }
-
-        let signInJS = document.createElement('script');
-        signInJS.async = async;
-        signInJS.src = src;
-        signInJS.onload = onload;
-        signInJS.text = inner_text_content; // LinkedIn
-        document.head.appendChild(signInJS);
+        // get document if platform is only browser
+        if (isPlatformBrowser(this.platformId) && !document.getElementById(id)) {
+            let signInJS = document.createElement('script');
+            signInJS.async = async;
+            signInJS.src = src;
+            signInJS.onload = onload;
+            signInJS.text = inner_text_content; // LinkedIn
+            document.head.appendChild(signInJS);
+        }
     }
 }
