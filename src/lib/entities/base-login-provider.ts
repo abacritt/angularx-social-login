aa26ad89e6ae8171bf7b6acccf16b88c95dcb821
Ruben Cid <rubencidlara@gmail.com>
2019-10-31 17:09:41 +0100

    check browser

--- src/lib/entities/base-login-provider.ts
+++ src/lib/entities/base-login-provider.ts
@@ -1,12 +1,14 @@
 import { LoginProvider } from './login-provider';
 import { SocialUser } from './user';
 import { BehaviorSubject } from 'rxjs';
+import {isPlatformBrowser} from '@angular/common';
+import {Inject, PLATFORM_ID} from '@angular/core';
 
 export abstract class BaseLoginProvider implements LoginProvider {
 
     protected _readyState: BehaviorSubject<boolean> = new BehaviorSubject(false);
 
-    constructor() { }
+    constructor(@Inject(PLATFORM_ID) private platformId: Object,) { }
 
     protected onReady(): Promise<void> {
         return new Promise((resolve, reject) => {
@@ -25,7 +27,7 @@ export abstract class BaseLoginProvider implements LoginProvider {
 
     loadScript(id: string, src: string, onload: any, async = true, inner_text_content = ''): void {
         // get document if platform is only browser
-        if (document && !document.getElementById(id)) {
+        if (isPlatformBrowser(this.platformId) && document && !document.getElementById(id)) {
             let signInJS = document.createElement('script');
             signInJS.async = async;
             signInJS.src = src;
