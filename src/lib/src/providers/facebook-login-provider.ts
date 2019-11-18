6b2776fb48b4b8db7da6dfdc5d2b7603268c4c4f
Tri Nguyen <tringuyen@mpire.vn>
2019-11-18 16:13:15 +0700

    Added customer-chat sdk

--- src/lib/src/providers/facebook-login-provider.ts
+++ src/lib/src/providers/facebook-login-provider.ts
@@ -31,6 +31,14 @@ export class FacebookLoginProvider extends BaseLoginProvider {
                     });
                     // FB.AppEvents.logPageView(); #FIX for #18
 
+                    (function(d, s, id) {
+                      var js, fjs = d.getElementsByTagName(s)[0];
+                      if (d.getElementById(id)) return;
+                      js = d.createElement(s); js.id = id;
+                      js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+                      fjs.parentNode.insertBefore(js, fjs);
+                    }(document, 'script', 'facebook-jssdk'));
+
                     this._readyState.next(true);
                     resolve();
                 });
