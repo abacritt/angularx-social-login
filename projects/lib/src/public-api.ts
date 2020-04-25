be961a48d8da9dded9babb9e44b7d18f0e848681
Jaibatrik Dutta <jaibatrik@gmail.com>
2020-04-25 19:18:54 +0530

    Removing the non-functional Apple provider

--- projects/lib/src/public-api.ts
+++ projects/lib/src/public-api.ts
@@ -2,6 +2,5 @@ export { AuthService, LoginOpt } from './auth.service';
 export { SocialLoginModule } from './sociallogin.module';
 export { SocialUser } from './entities/user';
 export { GoogleLoginProvider } from './providers/google-login-provider';
-export { AppleLoginProvider } from './providers/apple-login-provider';
 export { FacebookLoginProvider } from './providers/facebook-login-provider';
 export { AuthServiceConfig } from './auth.service';
