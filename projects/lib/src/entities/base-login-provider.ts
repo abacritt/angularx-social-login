bc55650a86b67c7e893e6ceac5d4f2e70a4b0024
Jaibatrik Dutta <jaibatrik@gmail.com>
2020-11-07 20:04:40 +0530

    refactor: New way of initialization for providers

--- projects/lib/src/entities/base-login-provider.ts
+++ projects/lib/src/entities/base-login-provider.ts
@@ -1,20 +1,54 @@
 import { LoginProvider } from './login-provider';
 import { SocialUser } from './social-user';
-import { BehaviorSubject } from 'rxjs';
+
+enum Status {
+  NOT_LOADED,
+  LOADING,
+  LOADED,
+}
 
 export abstract class BaseLoginProvider implements LoginProvider {
   constructor() {}
 
-  abstract initialize(): Promise<void>;
   abstract getLoginStatus(): Promise<SocialUser>;
   abstract signIn(): Promise<SocialUser>;
   abstract signOut(revoke?: boolean): Promise<any>;
 
+  protected readyState: Status = Status.NOT_LOADED;
+
+  protected initialize(
+    id: string,
+    src: string,
+    parentElement: HTMLElement = null,
+    onload: any
+  ): Promise<void> {
+    return new Promise((resolve, reject) => {
+      if (this.readyState === Status.LOADED) {
+        resolve();
+      }
+
+      if (this.readyState !== Status.LOADING) {
+        this.readyState = Status.LOADING;
+
+        try {
+          this.loadScript(id, src, parentElement, () => {
+            onload();
+
+            this.readyState = Status.LOADED;
+            resolve();
+          });
+        } catch (error) {
+          reject(error);
+        }
+      }
+    });
+  }
+
   protected loadScript(
     id: string,
     src: string,
-    onload: any,
-    parentElement = null
+    parentElement: HTMLElement = null,
+    onload: any
   ): void {
     // get document if platform is only browser
     if (typeof document !== 'undefined' && !document.getElementById(id)) {
--- projects/lib/src/entities/login-provider.ts
+++ projects/lib/src/entities/login-provider.ts
@@ -2,7 +2,6 @@ import { SocialUser } from './social-user';
 
 export interface LoginProvider {
   getLoginStatus(): Promise<SocialUser>;
-	signIn(signInOptions?: any): Promise<SocialUser>;
-	signOut(revoke?: boolean): Promise<any>;
+  signIn(signInOptions?: any): Promise<SocialUser>;
+  signOut(revoke?: boolean): Promise<any>;
 }
-
