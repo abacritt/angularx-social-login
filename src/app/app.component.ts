1c38cc07887697804c7a221cfd14a37024954576
Jaibatrik Dutta <jaibatrik@gmail.com>
2021-07-12 12:43:31 +0530

    ESLint integration (#432)
    * upgrade: Angular 12 CLI update
    
    * upgrade: Angular 12 core update
    
    * chore: ESLint integration
    
    * chore: ESLint integration
    
    * chore: ESLint integration
    
    * chore: configure lint-staged
    
    * chore: version update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'lib-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/demo/demo.component.spec.ts
+++ src/app/demo/demo.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DemoComponent } from './demo.component';
 
@@ -6,7 +6,7 @@ describe('DemoComponent', () => {
   let component: DemoComponent;
   let fixture: ComponentFixture<DemoComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ DemoComponent ]
     })
