b4b5b8ecee3909f9463b62bb1d2049ab62f1bf80
Mark Kariuki <kariukimarkg@gmail.com>
2022-08-12 23:51:50 +0300

    theme attribute and logo alignment

--- projects/lib/src/directives/google-signin-button.directive.ts
+++ projects/lib/src/directives/google-signin-button.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, Input} from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 import { take } from 'rxjs';
 import { SocialAuthService } from '../socialauth.service';
 
@@ -8,48 +8,59 @@ import { SocialAuthService } from '../socialauth.service';
 })
 export class GoogleSigninButtonDirective {
   @Input()
-  type : 'icon' | 'standard' = 'icon';
+  type: 'icon' | 'standard' = 'icon';
 
   @Input()
   size: 'small' | 'medium' | 'large' = 'medium';
-  
+
   @Input()
   text: 'signin_with' | 'signup_with' = 'signin_with';
 
   @Input()
-  shape: 
-  'square' | 'circle' | 'pill' | 'rectangular' = 'rectangular'
+  shape: 'square' | 'circle' | 'pill' | 'rectangular' = 'rectangular';
 
-  
   @Input()
-  width:any;
+  theme: 'outline' | 'filled_blue' | 'filled_black' = 'outline';
 
+  @Input()
+  logo_alignment: 'left' | 'center' = 'left';
 
-  constructor(
-    el: ElementRef,
-    socialAuthService: SocialAuthService
-  ) {
+  @Input()
+  width: string = '';
 
-    
+  @Input()
+  locale: string = '';
+
+  constructor(el: ElementRef, socialAuthService: SocialAuthService) {
     socialAuthService.initState.pipe(take(1)).subscribe(() => {
 
-      if(this.width > 400 || this.width < 40)
-      { 
-        Promise.reject(
-          'Please note .. max-width 400 , min-width 40 ' +
-            '(https://developers.google.com/identity/gsi/web/tools/configurator)'
-        );
-      }
 
-      else{
-      google.accounts.id.renderButton(el.nativeElement, {
-        type: this.type,
-        size: this.size,
-        text: this.text,
-        width: this.width,
-        shape: this.shape,
+      Promise.resolve(this.locale).then((value) => {
+
+        console.log("value", value);
+      })
+
+
+      Promise.resolve(this.width).then((value) => {
+        if (value > '400' || (value < '200' && value != '')) {
+          Promise.reject(
+            'Please note .. max-width 400 , min-width 200 ' +
+              '(https://developers.google.com/identity/gsi/web/tools/configurator)'
+          );
+        } else {
+          google.accounts.id.renderButton(el.nativeElement, {
+            type: this.type,
+            size: this.size,
+            text: this.text,
+            width: this.width,
+            shape: this.shape,
+            theme: this.theme,
+            logo_alignment: this.logo_alignment,
+            locale: this.locale,
+            
+          });
+        }
       });
-    }
-    })
+    });
   }
 }
