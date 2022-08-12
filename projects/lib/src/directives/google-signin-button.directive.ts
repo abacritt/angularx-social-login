008b9b9b6b2555e6c69eda0cdaff8e13ef50d1ea
Mark Kariuki <kariukimarkg@gmail.com>
2022-08-12 12:29:55 +0300

    added text change width adn shape of button

--- projects/lib/src/directives/google-signin-button.directive.ts
+++ projects/lib/src/directives/google-signin-button.directive.ts
@@ -12,16 +12,44 @@ export class GoogleSigninButtonDirective {
 
   @Input()
   size: 'small' | 'medium' | 'large' = 'medium';
+  
+  @Input()
+  text: 'signin_with' | 'signup_with' = 'signin_with';
+
+  @Input()
+  shape: 
+  'square' | 'circle' | 'pill' | 'rectangular' = 'rectangular'
+
+  
+  @Input()
+  width:any;
+
 
   constructor(
     el: ElementRef,
     socialAuthService: SocialAuthService
   ) {
+
+    
     socialAuthService.initState.pipe(take(1)).subscribe(() => {
+
+      if(this.width > 400 || this.width < 40)
+      { 
+        Promise.reject(
+          'Please note .. max-width 400 , min-width 40 ' +
+            '(https://developers.google.com/identity/gsi/web/tools/configurator)'
+        );
+      }
+
+      else{
       google.accounts.id.renderButton(el.nativeElement, {
         type: this.type,
         size: this.size,
+        text: this.text,
+        width: this.width,
+        shape: this.shape,
       });
+    }
     })
   }
 }
