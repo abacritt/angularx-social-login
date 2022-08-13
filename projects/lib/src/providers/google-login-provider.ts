a2efbab0e432d1dc2b3086b2743a67d4dda29940
kattoshi <urabe@mcomp.co.jp>
2022-08-13 14:00:38 +0900

    fixes "atob" decode failed to execute

--- projects/lib/src/providers/google-login-provider.ts
+++ projects/lib/src/providers/google-login-provider.ts
@@ -186,6 +186,16 @@ export class GoogleLoginProvider extends BaseLoginProvider {
   }
 
   private decodeJwt(idToken: string): Record<string, string | undefined> {
-    return JSON.parse(window.atob(idToken.split('.')[1]));
+    const base64Url = idToken.split(".")[1];
+    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    const jsonPayload = decodeURIComponent(
+      window.atob(base64)
+        .split("")
+        .map(function (c) {
+          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join("")
+    );
+    return JSON.parse(jsonPayload);
   }
 }
